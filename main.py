31cccd6a907eae8a1fb8053c443c3c0eeae32e1f
Pablo-Weverton-Dev <90657749+pablo-weverton@users.noreply.github.com>
2022-03-20 18:56:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 from turtle import Screen
-from snake import Snake
+from score_board import Score
 from food import Food
-from scoreboard import Scoreboard
 import time
+from snake import Snake
 
+move_on = True
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.bgcolor('black')
+screen.title("Welcome to The Snake Game")
 screen.tracer(0)
 
 snake = Snake()
 food = Food()
-scoreboard = Scoreboard()
+scoreboard = Score()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
-game_is_on = True
-while game_is_on:
+while move_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food
+
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.increase_score()
+        scoreboard.clear()
+        scoreboard.increase()
 
-    #Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    # Detect collision with wall
 
-    #Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    if snake.head.ycor() <= -300 or snake.head.ycor() >= 320:
+        scoreboard.lose()
+        break
 
+    if snake.head.xcor() <= -300 or snake.head.xcor() >= 300:
+        scoreboard.lose()
+        break
 
+    # Detect collision with own tail
 
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            move_on = False
+            scoreboard.lose()
 
+with open('data.txt', 'r') as file:
+    greatest_score = file.readline()
+    if int(scoreboard.score) > int(greatest_score):
+        with open('data.txt', 'w') as new_score:
+            new_score.write(f'{scoreboard.score}')
 
 screen.exitonclick()
