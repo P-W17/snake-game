09688f9460c26a4ed6a77b17766544a2a85139f1
Pablo-Weverton-Dev <90657749+pablo-weverton@users.noreply.github.com>
2022-03-20 18:55:41 -0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,27 +1,28 @@
+import time
 from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard(Turtle):
+class Score(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open('data.txt', 'r') as data:
+            self.greatest_score = int(data.readline())
         self.score = 0
-        self.color("white")
-        self.penup()
-        self.goto(0, 270)
+        self.color('white')
         self.hideturtle()
-        self.update_scoreboard()
+        self.penup()
+        self.goto(x=0, y=250)
+        self.write(f'Score : {self.score}', align='center', font=('Arial', 24, 'normal'))
 
-    def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
-    def increase_score(self):
+    def increase(self):
         self.score += 1
+        self.write(f'Score : {self.score}', align='center', font=('Arial', 24, 'normal'))
+
+    def lose(self):
+        self.speed('fastest')
+        self.goto(0, 0)
+        self.write('Game Over', align='center', font=('arial', 30, 'normal'))
+        time.sleep(2)
         self.clear()
-        self.update_scoreboard()
+        self.write(f'Greatest Score: {self.greatest_score}', align='center', font=('arial', 30, 'normal'))
