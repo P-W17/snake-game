c45ef3fdceaaa84718868eef9532db97cee4981c
Pablo-Weverton-Dev <90657749+pablo-weverton@users.noreply.github.com>
2022-03-20 18:56:59 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,10 +1,12 @@
 from turtle import Turtle
+
+move_on = True
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+MOVE_DISTANCE = 20
 
 
 class Snake:
